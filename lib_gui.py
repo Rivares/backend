d0a9ebb686e9d5915de889427e2bb43cfe0c9721
TatianaEtu <stm1995@mail.ru>
2020-12-24 19:29:22 +0300

    Kivy_3

--- lib_gui.py
+++ lib_gui.py
@@ -3,6 +3,7 @@
 import lib_general as my_general
 
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.label import Label
@@ -24,36 +25,94 @@ purple = [1, 0, 1, 1]
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+        # label = Label(text='Hello from Kivy',
+        #               size_hint=(.5, .5),
+        #               pos_hint={'center_x': .5, 'center_y': .5})
+        #
+        # img = Image(source='/path/to/real_python.png',
+        #             size_hint=(1, .5),
+        #             pos_hint={'center_x': .5, 'center_y': .5})
+        #
+        # layout = BoxLayout(padding=10)
+        # colors = [red, green, blue, purple]
+        #
+        # # padding: Отступ padding между лейаутом и его дочерними элементами уточняется в пикселях.\
+        # # Для этого можно выбрать один из трех способов:
+        # # Список из четырех аргументов: [padding_left, padding_top, padding_right, padding_bottom]
+        # # Список из двух аргументов: [padding_horizontal, padding_vertical]
+        # # Один аргумент:
+        # # padding = 10 spacing: При помощи данного аргумента добавляется расстояние между дочерними виджетами.
+        # # orientation: Позволяет изменить значение orientation для BoxLayout по умолчанию — с горизонтального на вертикальное.
+        #
+        # for i in range(5):
+        #     btn = Button(text="Button #%s" % (i + 1),
+        #                  background_color=my_general.random.choice(colors),
+        #                  size_hint=(.5, .5),
+        #                  pos_hint={'center_x': .5, 'center_y': .5})
+        #
+        #     btn.bind(on_press=self.on_press_button)
+        #
+        #     layout.add_widget(btn)
 
-        img = Image(source='/path/to/real_python.png',
-                    size_hint=(1, .5),
-                    pos_hint={'center_x': .5, 'center_y': .5})
+        self.operators = ["/", "*", "+", "-"]
+        self.last_was_operator = None
+        self.last_button = None
+        main_layout = BoxLayout(orientation="vertical")
+        self.solution = TextInput(
+            multiline=False, readonly=True, halign="right", font_size=55
+        )
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "C", "+"],
+        ]
+        for row in buttons:
+            h_layout = BoxLayout()
+            for label in row:
+                button = Button(
+                    text=label,
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                )
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
 
-        layout = BoxLayout(padding=10)
-        colors = [red, green, blue, purple]
+        equals_button = Button(
+            text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
+        )
+        equals_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equals_button)
 
-        # padding: Отступ padding между лейаутом и его дочерними элементами уточняется в пикселях.\
-        # Для этого можно выбрать один из трех способов:
-        # Список из четырех аргументов: [padding_left, padding_top, padding_right, padding_bottom]
-        # Список из двух аргументов: [padding_horizontal, padding_vertical]
-        # Один аргумент:
-        # padding = 10 spacing: При помощи данного аргумента добавляется расстояние между дочерними виджетами.
-        # orientation: Позволяет изменить значение orientation для BoxLayout по умолчанию — с горизонтального на вертикальное.
+        return main_layout
 
-        for i in range(5):
-            btn = Button(text="Button #%s" % (i + 1),
-                         background_color=my_general.random.choice(colors),
-                         size_hint=(.5, .5),
-                         pos_hint={'center_x': .5, 'center_y': .5})
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
 
-            btn.bind(on_press=self.on_press_button)
+        if button_text == "C":
+            # Очистка виджета с решением
+            self.solution.text = ""
+        else:
+            if current and (
+                self.last_was_operator and button_text in self.operators):
+                # Не добавляйте два оператора подряд, рядом друг с другом
+                return
+            elif current == "" and button_text in self.operators:
+                # Первый символ не может быть оператором
+                return
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+        self.last_button = button_text
+        self.last_was_operator = self.last_button in self.operators
 
-            layout.add_widget(btn)
+    def on_solution(self, instance):
+        text = self.solution.text
+        if text:
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
 
     def on_press_button(self, instance):
         print('Вы нажали на кнопку!')
-
-        return label
