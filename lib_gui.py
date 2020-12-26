5697ef8d84a5aa6b724720e0a6e8d203d0ecce44
TatianaEtu <stm1995@mail.ru>
2020-12-27 01:31:22 +0300

    GUI_6

--- lib_gui.py
+++ lib_gui.py
@@ -228,11 +228,12 @@ class MainScreen(Screen):
         boxlayout_col_0.add_widget(deferred_orders)
 
         switch_panel_to_screen_3 = Button(
-            text="Explanations for notes",
+            text="Doubler Screen -->",
             background_color=[0, 1, 0, 1],
-            size_hint=[1, 0.15]
+            size_hint=[1, 0.15],
             # width=200,
             # height=10,
+            on_press=self._on_press_button_to_doubler_screen
         )
 
         btn_sign_out = Button(
@@ -278,6 +279,11 @@ class MainScreen(Screen):
 
         self.add_widget(boxlayout_gen)
 
+    def _on_press_button_to_doubler_screen(self, *args):
+
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'DoublerScreen'
+
     def _on_press_button_sign_out(self, *args):
 
         Window.fullscreen = False
@@ -287,10 +293,110 @@ class MainScreen(Screen):
         self.manager.current = 'PasswordScreen'
 
 
+class DoublerScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        boxlayout_gen = BoxLayout(orientation='horizontal', spacing=10)
+
+        boxlayout_col_0 = BoxLayout(orientation='vertical', spacing=10)
+        boxlayout_col_1 = BoxLayout(orientation='vertical', spacing=10)
+        boxlayout_row_0 = BoxLayout(orientation='horizontal', spacing=10)
+        boxlayout_row_1 = BoxLayout(orientation='horizontal', spacing=10)
+        boxlayout_grph = BoxLayout(orientation='horizontal', spacing=10)
+
+        print("DoublerScreen")
+
+        glass = Button(
+            text="Glass",
+            background_color=[.50, 0, 0, 1],
+            size_hint=[.9, 0.1],
+        )
+
+        list_operations = Button(
+            text="List operations",
+            background_color=[0, .50, 0, 1],
+            size_hint=[.9, 0.1]
+        )
+
+        deferred_orders = Button(
+            text="Deferred orders",
+            background_color=[0, 0, .50, 1],
+            size_hint=[.9, 0.1]
+        )
+
+        boxlayout_col_0.add_widget(glass)
+        boxlayout_col_0.add_widget(list_operations)
+        boxlayout_col_0.add_widget(deferred_orders)
+
+        switch_panel_to_screen_3 = Button(
+            text="<-- Main Screen",
+            background_color=[0, 1, 0, 1],
+            size_hint=[1, 0.15],
+            # width=200,
+            # height=10,
+            on_press=self._on_press_button_to_main_screen
+        )
+
+        btn_sign_out = Button(
+            text="Sign out",
+            background_color=[1, 0, 0, 1],
+            size_hint=[1, 0.15],
+            # width=70,
+            # height=10,
+            # pos=(400, 40),
+            on_press=self._on_press_button_sign_out
+        )
+
+        boxlayout_row_0.add_widget(switch_panel_to_screen_3)
+        boxlayout_row_0.add_widget(btn_sign_out)
+        boxlayout_col_1.add_widget(boxlayout_row_0)
+
+        my_general.plt.plot([1, 23, 2, 4])
+        my_general.plt.ylabel('some numbers')
+        boxlayout_grph.add_widget(FigureCanvasKivyAgg(my_general.plt.gcf()))
+
+        boxlayout_grph.size = (500, 500)
+
+        boxlayout_col_1.add_widget(boxlayout_grph)
+        active_orders = Button(
+            text="Active orders",
+            background_color=[0, .50, 0, 1],
+            size_hint=[1, 0.5]
+        )
+
+        explanations_notes = Button(
+            text="Explanations for notes",
+            background_color=[0, 0, .50, 1],
+            size_hint=[1, 0.5]
+        )
+        boxlayout_row_1.add_widget(active_orders)
+        boxlayout_row_1.add_widget(explanations_notes)
+
+        boxlayout_col_1.add_widget(boxlayout_row_1)
+        boxlayout_col_1.size_hint = [5, 1.4]
+
+        boxlayout_gen.add_widget(boxlayout_col_0)
+        boxlayout_gen.add_widget(boxlayout_col_1)
+
+        self.add_widget(boxlayout_gen)
+
+    def _on_press_button_to_main_screen(self, *args):
+
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'MainScreen'
+
+    def _on_press_button_sign_out(self, *args):
+
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'PasswordScreen'
+
+
 class Investment_analysis(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(PasswordScreen(name='PasswordScreen'))
         sm.add_widget(MainScreen(name='MainScreen'))
+        sm.add_widget(DoublerScreen(name='DoublerScreen'))
 
         return sm
